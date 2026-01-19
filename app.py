bc9b14d38ea5f40252656101a5a8948605d843e4
GAYOSM <146345000+GAYOSM@users.noreply.github.com>
2026-01-19 15:14:31 +0530

    Chikken Chilli added

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ DB_NAME = "restaurant.db"
 MENU_ITEMS = [
     "Porotta", "Dosa", "Idiyappam", "Chappathi",
     "Chicken Fry half", "Chicken Fry half", "Chicken Curry half", "Chicken Curry Full", "Beef Fry", "Beef Curry",
-    "Kanthari Piece", "Kurumma",
+    "Kanthari Piece", "Chikken Chilli", "Kurumma",
     "Set Bulsey", "Single Omblet", "Double Omblet",
     "Kanthari Combo", "49/- Combo", "Chicken fry Combo", "Beef Chapse Combo", "Pazhampori Combo",
     "Drink (20)", "Drink (40)", "Lime", 
